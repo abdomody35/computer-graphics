7928c5d5fb7850f2213f47d2c0d3649e71948bf6
abdomody35 <abdomody35@gmail.com>
2025-10-24 16:53:54 +0300

    feat(line): add DDA line drawing algorithm and ignore output PNGs
    - Implement Digital Differential Analyzer algorithm in dda.cpp for raster line generation
    - Add *.png to .gitignore to exclude generated image files from version control
    - Include sample image creation in main() demonstrating line drawing functionality

---
+++ line/dda.cpp
@@ -0,0 +1,73 @@
+#include "../Image.h"
+
+struct Point {
+    int x;
+    int y;
+};
+
+void drawLineDDA(GrayscaleImage &image, Point p1, Point p2, Byte color = 255)
+{
+    float m = std::fabs((float)(p2.y - p1.y) / (float)(p2.x - p1.x));
+
+    if (m < 1)
+    {
+        if (p1.x > p2.x)
+        {
+            std::swap(p1, p2);
+        }
+
+        float y = p1.y;
+
+        for (int x = p1.x; x <= p2.x; x++)
+        {
+            image(x, std::round(y)) = color;
+
+            if (p1.y < p2.y)
+            {
+                y += m;
+            }
+            else
+            {
+                y -= m;
+            }
+        }
+    }
+    else
+    {
+        if (p1.y > p2.y)
+        {
+            std::swap(p1, p2);
+        }
+
+        float m_inv = 1 / m;
+
+        float x = p1.x;
+
+        for (int y = p1.y; y <= p2.y; y++)
+        {
+            image(std::round(x), y) = color;
+
+            if (p1.x < p2.x)
+            {
+                x += m_inv;
+            }
+            else
+            {
+                x -= m_inv;
+            }
+        }
+    }
+}
+
+int main()
+{
+    GrayscaleImage image(255, 255);
+
+    Point p1 = { 50, 20 }, p2 = { 80, 100 };
+
+    drawLineDDA(image, p2, p1);
+
+    image.Save("DDA.png");
+
+    return 0;
+}
