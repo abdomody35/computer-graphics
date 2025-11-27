e5e78247725a5e8e51627e5b7834daacd2140950
abdomody35 <abdomody35@gmail.com>
2025-11-27 10:52:42 +0200

    feat(circle): add fill option to Bresenham and Midpoint circle drawing

--- circle/bresenham.cpp
+++ circle/bresenham.cpp
@@ -17,7 +17,7 @@ void setPixel(GrayscaleImage &image, int x, int y, Byte color)
     }
 }
 
-void drawCircleBresenham(GrayscaleImage &image, const Point &center, int radius, Byte color = 255)
+void drawCircleBresenham(GrayscaleImage &image, const Point &center, int radius, bool fill = false, Byte color = 255)
 {
     if (radius <= 0)
     {
@@ -31,14 +31,43 @@ void drawCircleBresenham(GrayscaleImage &image, const Point &center, int radius,
 
     while (y <= x)
     {
-        setPixel(image, center.x + x, center.y + y, color);
-        setPixel(image, center.x - x, center.y + y, color);
-        setPixel(image, center.x + x, center.y - y, color);
-        setPixel(image, center.x - x, center.y - y, color);
-        setPixel(image, center.x + y, center.y + x, color);
-        setPixel(image, center.x - y, center.y + x, color);
-        setPixel(image, center.x + y, center.y - x, color);
-        setPixel(image, center.x - y, center.y - x, color);
+        Point o1 = {center.x + x, center.y + y};
+        Point o2 = {center.x - x, center.y + y};
+        Point o3 = {center.x + x, center.y - y};
+        Point o4 = {center.x - x, center.y - y};
+        Point o5 = {center.x + y, center.y + x};
+        Point o6 = {center.x - y, center.y + x};
+        Point o7 = {center.x + y, center.y - x};
+        Point o8 = {center.x - y, center.y - x};
+
+        setPixel(image, o1.x, o1.y, color);
+        setPixel(image, o2.x, o2.y, color);
+        setPixel(image, o3.x, o3.y, color);
+        setPixel(image, o4.x, o4.y, color);
+        setPixel(image, o5.x, o5.y, color);
+        setPixel(image, o6.x, o6.y, color);
+        setPixel(image, o7.x, o7.y, color);
+        setPixel(image, o8.x, o8.y, color);
+
+        if (fill)
+        {
+            for (int i = o2.x; i <= o1.x; i++)
+            {
+                setPixel(image, i, o1.y, color);
+            }
+            for (int i = o4.x; i <= o3.x; i++)
+            {
+                setPixel(image, i, o3.y, color);
+            }
+            for (int i = o6.x; i <= o5.x; i++)
+            {
+                setPixel(image, i, o5.y, color);
+            }
+            for (int i = o8.x; i <= o7.x; i++)
+            {
+                setPixel(image, i, o7.y, color);
+            }
+        }
 
         y++;
 
@@ -62,6 +91,7 @@ int main()
 
     int radius = 100;
 
+    drawCircleBresenham(image, center, radius, true, 128);
     drawCircleBresenham(image, center, radius);
 
     image.Save("circleBresenham.png");
